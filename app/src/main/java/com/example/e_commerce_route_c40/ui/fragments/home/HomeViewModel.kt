aaf3e5f69268de39a25401aa5add0613e7759de7
Abdullah Bendary <141359699+Abdullah-BenDarY@users.noreply.github.com>
2024-09-08 21:08:03 +0300

    Feat : implement brands viewModel

--- app/src/main/java/com/example/e_commerce_route_c40/ui/fragments/home/HomeViewModel.kt
+++ app/src/main/java/com/example/e_commerce_route_c40/ui/fragments/home/HomeViewModel.kt
@@ -3,7 +3,9 @@ package com.example.e_commerce_route_c40.ui.fragments.home
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.e_commerce_route_c40.base.BaseViewModel
+import com.route.domain.model.Brand
 import com.route.domain.model.Category
+import com.route.domain.usecase.GetBrandsUseCase
 import com.route.domain.usecase.GetCategoriesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -12,9 +14,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getCategoriesUseCase: GetCategoriesUseCase
+    private val getCategoriesUseCase: GetCategoriesUseCase,
+    private val getBrandsUseCase: GetBrandsUseCase
 ) : BaseViewModel() {
     val categoriesLiveData = MutableLiveData<List<Category>?>()
+    val brandsLiveData = MutableLiveData<List<Brand>?>()
+
     fun getCategoryList() {
         try {
             viewModelScope.launch (Dispatchers.IO){
@@ -23,6 +28,19 @@ class HomeViewModel @Inject constructor(
             }
         }catch (e:Exception){
             handleError(e)
+        }
+
+    }
+
+    fun getBrandsList() {
+        try {
+            viewModelScope.launch (Dispatchers.IO){
+                val brands = getBrandsUseCase.invoke()
+                brandsLiveData.postValue(brands)
+            }
+        }catch (e:Exception){
+            handleError(e)
+
         }
 
     }
